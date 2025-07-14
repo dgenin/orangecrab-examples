5eb6417f578f616a202853fefda7e4776c8942bc
Dan Genin <dan.genin@gmail.com>
2025-07-14 18:58:17 -0400

    Almost working multipler for two 16 bit numbers
    The high byte is old data

--- verilog/usb_acm_device/usb_acm_device.v
+++ verilog/usb_acm_device/usb_acm_device.v
@@ -23,29 +23,21 @@ module usb_acm_device (
     reg [2:0] reg_counter = 0;
 
     // Getting an extra character at the start of a burst 
-    always @(posedge clk48) begin
-        if (uart_in_valid == 1 & reset == 0) begin
+    always @(negedge clk48) begin
+          if ((uart_in_valid == 1) && (uart_in_ready == 1)) begin
             case (reg_counter)
-//                3'd0 : begin reg0[15:8] <= uart_out_data; uart_in_data <= uart_out_data; end
-//                3'd1 : begin reg0[7:0] <= uart_out_data; uart_in_data <= uart_out_data; end
-//                3'd2 : begin reg1[15:8] <= uart_out_data; uart_in_data <= uart_out_data; end
-//                3'd3 : begin reg1[7:0] <= uart_out_data; uart_in_data <= uart_out_data; end
-                3'd0 : begin reg0[15:8] <= uart_out_data; uart_in_data <= 8'h50; end
-                3'd1 : begin reg0[7:0] <= uart_out_data; uart_in_data <= 8'h51; end
-                3'd2 : begin reg1[15:8] <= uart_out_data; uart_in_data <= 8'h52; end
-                3'd3 : begin reg1[7:0] <= uart_out_data; uart_in_data <= 8'h53; end
-                3'd4 : begin out_reg <= reg0*reg1; uart_in_data[7:3] <= 5'hA; uart_in_data[3:0] <= reg_counter; end
+                3'd0 : begin reg0[15:8] <= uart_out_data; uart_in_data <= uart_out_data; end
+                3'd1 : begin reg0[7:0] <= uart_out_data; uart_in_data <= uart_out_data; end
+                3'd2 : begin reg1[15:8] <= uart_out_data; uart_in_data <= uart_out_data; end
+                3'd3 : begin reg1[7:0] <= uart_out_data; uart_in_data <= uart_out_data; end
+                // The multiplier can't keep up so the highest byte is sent a full `always` cycle later
+                3'd4 : begin out_reg <= reg0*reg1; uart_in_data <= out_reg[31:24];  end
 //                3'd5 : uart_in_data <= out_reg[31:24];
                 3'd5 : uart_in_data <= out_reg[23:16];
                 3'd6 : uart_in_data <= out_reg[15:8];
                 3'd7 : uart_in_data <= out_reg[7:0];
             endcase 
-//            if (uart_out_data == 8'd10) begin
-//                reg_counter <= 0;
-//            end else begin
-//                reg_counter <= reg_counter + 1;
-//            end
-            reg_counter = reg_counter + 1;
+            reg_counter <= reg_counter + 1;
         end
     end
         
@@ -79,8 +71,8 @@ module usb_acm_device (
     // assign uart_in_data[7:2] = 6'd16;
     // assign uart_in_data = reg0;
     // assign uart_out_data = uart_in_data;
-    wire       uart_in_valid;
-    wire       uart_in_ready;
+    wire uart_in_valid;
+    wire uart_in_ready;
 
 
     wire usb_p_in;
@@ -122,8 +114,5 @@ module usb_acm_device (
 
     BB io_p( .I( usb_p_tx ), .T( !usb_tx_en ), .O( usb_p_in ), .B( usb_d_p ) );
     BB io_n( .I( usb_n_tx ), .T( !usb_tx_en ), .O( usb_n_in ), .B( usb_d_n ) );
-
-// assign usb_p_in = 1'b0;
-// assign usb_n_in = 1'b0  ;
 
 endmodule
